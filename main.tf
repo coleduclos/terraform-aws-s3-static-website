7862be33431bdd6d56a5896846c770292a94ea14
Cole Duclos <cole.duclos@autodesk.com>
2023-02-18 09:46:12 -0700

    removing certificate_arn variable

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_cloudfront_distribution" "website_root_distribution" {
     }
 
     viewer_certificate {
-        acm_certificate_arn = var.certificate_arn
+        acm_certificate_arn = module.acm.acm_certificate_arn
         ssl_support_method  = "sni-only"
         minimum_protocol_version = "TLSv1.2_2021"
     }
